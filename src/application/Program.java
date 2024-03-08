5bcd35c8568754874acbb6d905f50499673b1beb
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-03-08 15:34:11 -0300

    Utilização do Object, mas não é a solução definitiva

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,24 +2,29 @@ package application;
 
 import java.util.Scanner;
 
-import service.PrintServiceString;
+import service.PrintService;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		PrintServiceString ps = new PrintServiceString();
+		PrintService ps = new PrintService();
 
 		System.out.print("How many values?");
 		int n = sc.nextInt();
+		
+		ps.addValue("Maria");   // Object aceita todo tipo, mas nem sempre é uma boa solução
 
 		for (int i = 0; i < n; i++) {
-			String value = sc.next();
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
 
 		ps.print();
+		Integer x = (Integer) ps.first();     /* Quando se utiliza o Object, ele até aceita  
+		                                      o String e o Integer ao mesmo tempo, mas da erro. 
+		                                      Problemas como type safety e performace */
 		System.out.println("First: " + ps.first());
 
 		sc.close();
